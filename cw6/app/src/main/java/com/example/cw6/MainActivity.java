2b9140ae61fbc977e1581c064634d5d69e98e332
q8Retaj <q8retaj2005@gmail.com>
2022-08-18 01:10:51 +0300

    done

---
+++ cw6/app/src/main/java/com/example/cw6/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.cw6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Student> studentArrayList=new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView studentphotoView=findViewById(R.id.imageView);
+        TextView textView=findViewById(R.id.textView3);
+        TextView textView1=findViewById(R.id.textView);
+        TextView textView2=findViewById(R.id.textView4);
+        Button button=findViewById(R.id.button);
+        Student student1=new Student("Retaj",17,12,R.drawable.retaj);
+        Student student2=new Student("Youssef",14,12,R.drawable.youssef);
+        Student student3=new Student("Majed",15,11,R.drawable.majed);
+        Student student4=new Student("Salman",13,10,R.drawable.salman);
+
+        studentArrayList.add(student1);
+        studentArrayList.add(student2);
+        studentArrayList.add(student3);
+        studentArrayList.add(student4);
+
+        studentphotoView.setImageResource(studentArrayList.get(0).getStudentImg());
+        textView.setText(studentArrayList.get(0).getStudentName());
+        textView1.setText(String.valueOf(studentArrayList.get(0).getStudentAge()));
+        textView2.setText(String.valueOf(studentArrayList.get(0).getStudentGrade()));
+
+    }
+
+}
